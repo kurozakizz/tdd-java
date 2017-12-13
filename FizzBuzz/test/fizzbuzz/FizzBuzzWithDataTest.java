7244a426d5bd9b126c3230cbf3da36aa6785c878
kurozakizz <squallcope@gmail.com>
2017-12-13 10:43:11 +0700

    Remove constructor

--- FizzBuzz/test/fizzbuzz/FizzBuzzWithDataTest.java
+++ FizzBuzz/test/fizzbuzz/FizzBuzzWithDataTest.java
@@ -6,20 +6,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class FizzBuzzWithDataTest {
     
-    private int input;
-    private String expectedResult;
+    @Parameter
+    public int input;
+    @Parameter(1)
+    public String expectedResult;
     
-    public FizzBuzzWithDataTest(int input, String expectedResult) {
-        this.input = input;
-        this.expectedResult = expectedResult;
-    }
     
-    @Parameters
+    @Parameters(name = "{index}: say({0}) =>\"{1}\"")
     public static Collection<Object[]> setupTestData() {
         return Arrays.asList(new Object[][]{
             {1, "1"},
