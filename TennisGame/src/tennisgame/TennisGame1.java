a3ba6e5e06de231527c26725d401c6398636b4a0
kurozakizz <squallcope@gmail.com>
2017-12-13 14:23:38 +0700

    Refactor tennis game

--- TennisGame/src/tennisgame/TennisGame1.java
+++ TennisGame/src/tennisgame/TennisGame1.java
@@ -1,76 +1,59 @@
 package tennisgame;
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private final String player1Name;
+    private final String player2Name;
+
+    private final String[] scores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
+    @Override
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) {
+            player1Score += 1;
+        } else {
+            player2Score += 1;
+        }
     }
 
+    private String setWinner(String playerName) {
+        return "Win for " + playerName;
+    }
+
+    private String setAdvantage(String playerName) {
+        return "Advantage " + playerName;
+    }
+
+    @Override
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
+
+        final boolean equalScore = (player1Score == player2Score);
+        final boolean morethanThirtyScore = player1Score >= 4 || player2Score >= 4;
+
+        if (equalScore) {
+            if (player1Score <= 2) {
+                score = scores[player1Score] + "-All";
+            } else {
+                score = "Deuce";
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+        } else if (morethanThirtyScore) {
+            int scoreDifferent = Math.abs(player1Score - player2Score);
+            String advantagePlayer = player1Score > player2Score ? player1Name : player2Name;
+            if (scoreDifferent == 1) {
+                score = setAdvantage(advantagePlayer);
+            } else {
+                score = setWinner(advantagePlayer);
             }
+        } else {
+            score = scores[player1Score] + "-" + scores[player2Score];
         }
         return score;
     }
