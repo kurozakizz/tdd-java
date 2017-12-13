7a22983522fa73b1398ff1164c16a6374dddde02
kurozakizz <squallcope@gmail.com>
2017-12-13 10:22:29 +0700

    Refactor test case

--- FizzBuzz/test/fizzbuzz/FizzBuzzTest.java
+++ FizzBuzz/test/fizzbuzz/FizzBuzzTest.java
@@ -7,62 +7,31 @@ public class FizzBuzzTest {
     FizzBuzz fizzbuzz = new FizzBuzz();
     
     @Test
-    public void Test_Input1_Expected1 () {
+    public void saySame () {
         assertEquals("1", fizzbuzz.Say(1));
-    }
-    
-    @Test
-    public void Test_Input2_Expected2 () {
         assertEquals("2", fizzbuzz.Say(2));
-    }
-    
-    @Test
-    public void Test_Input3_ExpectedFizz () {
-        assertEquals("Fizz", fizzbuzz.Say(3));
-    }
-    
-    @Test
-    public void Test_Input4_Expected4 () {
         assertEquals("4", fizzbuzz.Say(4));
-    }
-    
-    @Test
-    public void Test_Input5_ExpectedBuzz () {
-        assertEquals("Buzz", fizzbuzz.Say(5));
-    }
-    
-    @Test
-    public void Test_Input6_ExpectedFizz () {
-        assertEquals("Fizz", fizzbuzz.Say(6));
-    }
-    
-    @Test
-    public void Test_Input7_Expected7 () {
         assertEquals("7", fizzbuzz.Say(7));
-    }
-    
-    @Test
-    public void Test_Input8_Expected8 () {
         assertEquals("8", fizzbuzz.Say(8));
     }
     
+    
     @Test
-    public void Test_Input9_ExpectedFizz () {
+    public void sayFizz () {
+        assertEquals("Fizz", fizzbuzz.Say(3));
+        assertEquals("Fizz", fizzbuzz.Say(6));
         assertEquals("Fizz", fizzbuzz.Say(9));
     }
     
     @Test
-    public void Test_Input10_ExpectedBuzz () {
+    public void sayBuzz () {
+        assertEquals("Buzz", fizzbuzz.Say(5));
         assertEquals("Buzz", fizzbuzz.Say(10));
     }
     
     @Test
-    public void Test_Input15_ExpectedFizzBuzz () {
+    public void sayFizzBuzz () {
         assertEquals("FizzBuzz", fizzbuzz.Say(15));
-    }
-    
-    @Test
-    public void Test_Input30_ExpectedFizzBuzz () {
         assertEquals("FizzBuzz", fizzbuzz.Say(30));
     }
     
